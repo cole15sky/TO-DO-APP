ec88e007bc6b8e0422707691dfec503d2386cebd
Sky Blue <dawadiaakash@gmail.com>
2024-11-11 10:48:54 +0545

    style: set default bg-white

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/global.css'],
 })
