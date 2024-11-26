e446005b4b7abac2a036a583970b3537859517da
Sky Blue <dawadiaakash@gmail.com>
2024-11-26 10:05:10 +0545

    style: add button emo, update li font size

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -81,7 +81,7 @@ onMounted(fetchTasks);
 
       <div class="flex items-center justify-between mb-6 bg-black p-3 rounded-full">
         <input v-model="taskInput" type="text" placeholder="Write the item here!"
-          class="main-box  text-black flex-1 p-3 bg-transparent border-none outline-none" />
+          class="  text-black flex-1 p-3 bg-transparent border-none outline-none" />
         <button @click="addTask" class="bg-blue-500 hover:bg-blue-800 p-3 w-[80px] rounded-full cursor-pointer ml-3">
           Add
         </button>
@@ -98,7 +98,7 @@ onMounted(fetchTasks);
             </div>
           </div>
 
-          <div v-else @click="toggleTaskCompletion(task)" class="task-content cursor-pointer">
+          <div v-else @click="toggleTaskCompletion(task)" class="task-content text-xl cursor-pointer">
            {{ task.title }}
             <span @click.stop="editTask(task)"
               class="absolute right-12 top-1/2 transform -translate-y-1/2 text-xl text-blue cursor-pointer">
@@ -120,7 +120,6 @@ onMounted(fetchTasks);
   background-color: gray;
   border-radius: 8px;
 }
-
 ul li {
   list-style: decimal;
   position: relative;
